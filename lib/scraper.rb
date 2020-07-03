195faa33723b86f68cd3e6557679643fba1258e2
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-03 17:16:04 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,15 +9,6 @@ class Scraper
   def get_page 
     doc = 
       Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-      
-    #doc.css(".post").each do |post|
-      #course = Course.new
-      #course.title = post.css("h2").text
-      #course.schedule = post.css(".date").text
-      #course.description = post.css("p").text
-    #end
-      
-
   end 
   
 end 
@@ -28,6 +19,15 @@ end
   
   
   Scraper.new.get_page
+  
+    def make_courses
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
+  end
   
   def print_courses
     self.make_courses
